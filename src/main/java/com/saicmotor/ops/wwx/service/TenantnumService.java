63cc9b7fd42ec9f60f1793f26cf4543c87ecf3dc
Jinjin123 <1293813551@qq.com>
2017-12-13 12:23:18 +0800

    add get tenant number

---
+++ src/main/java/com/saicmotor/ops/wwx/service/TenantnumService.java
@@ -0,0 +1,10 @@
+package com.saicmotor.ops.wwx.service;
+
+import java.util.Map;
+
+/**
+ * Created by Jimmy on 2017/12/13.
+ */
+public interface  TenantnumService {
+    Map<String,Object> gettenantnum(String length) throws Exception;
+}
